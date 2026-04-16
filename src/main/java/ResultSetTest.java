d58e72365c4c159633750fb4bf1c444460db63d5
eddie1031 <archaco123@gmail.com>
2026-04-16 14:25:43 +0900

    ~PrepaerdStatement

---
+++ src/main/java/ResultSetTest.java
@@ -0,0 +1,58 @@
+import util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ResultSetTest {
+
+    static void main() {
+
+        try (Connection conn = ConnectionUtil.MySql.getConnection()) {
+
+            String selectAllProductSql = """
+                    select
+                        *
+                    from
+                        product
+                    """;
+
+            PreparedStatement multipleResultStmt = conn.prepareStatement(selectAllProductSql);
+            ResultSet multipleResultSet = multipleResultStmt.executeQuery();
+
+            // Iterator
+            while(multipleResultSet.next()) {
+
+                long id = multipleResultSet.getLong("id");
+                String name = multipleResultSet.getString("name");
+                int price = multipleResultSet.getInt("price");
+                int stock = multipleResultSet.getInt("stock");
+
+                System.out.printf("%d번 상품 %s, 가격: %d, 재고: %d\n", id, name, price, stock);
+
+            }
+            System.out.println();
+
+            multipleResultSet.close();
+            multipleResultStmt.close();
+
+            long targetMemberId = 1L;
+
+            String selectSpecificProductQuery = """
+                    select
+                        *
+                    from
+                        product
+                    where
+                        id = ?
+                    """;
+
+
+        } catch ( Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
