60c5a1073b7949f74d38072a9aa94cb7e58e30b2
eddie1031 <archaco123@gmail.com>
2026-04-16 11:34:05 +0900

    ~sql injection

--- src/main/java/MySqlConnectionTest.java
+++ src/main/java/MySqlConnectionTest.java
@@ -1,17 +1,63 @@
 import util.ConnectionUtil;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 void main() {
 
     try (Connection conn = ConnectionUtil.MySql.getConnection()) {
 
-        String name = "김민수";
-        String pwd = "1234";
-        String email = "user1@example.com";
-        String balance = "10000";
+        String insertMemberSql = genMemberInsertQuery("김민수", "1234", "user1@example.com", "10000");
 
-        String memberInsert = genMemberInsertQuery(name, pwd, email, balance);
+        Statement stmt = conn.createStatement();
+
+//        // execute()
+//        boolean executeResult = stmt.execute(insertMemberSql);
+//
+//        if ( !executeResult ) {
+//            int affectedRowCounter = stmt.getUpdateCount();
+//            System.out.println("affectedRowCounter = " + affectedRowCounter);
+//        }
+
+        // executeUpdate()
+//        int affectedRowCount2 = stmt.executeUpdate(
+//                genMemberInsertQuery(
+//                        "박민준",
+//                        "5678",
+//                        "user2@example.com",
+//                        "30000"
+//                )
+//        );
+//
+//        System.out.println("총 %d개의 행이 영향을 받았습니다.".formatted(affectedRowCount2));
+
+
+        ResultSet selectMemberResultSet = stmt.executeQuery("""
+        select
+            *
+        from
+            member
+        """);
+
+        while(selectMemberResultSet.next()) {
+            System.out.println("""
+            id = %d,
+            name = %s,
+            password = %s,
+            email = %s,
+            balance = %s
+            """.formatted(
+                    selectMemberResultSet.getLong("id"),
+                    selectMemberResultSet.getString("name"),
+                    selectMemberResultSet.getString("password"),
+                    selectMemberResultSet.getString("email"),
+                    selectMemberResultSet.getInt("balance")
+            ));
+        }
+        selectMemberResultSet.close();
+
+        stmt.close();
 
     } catch ( Exception e ) {
 
@@ -19,7 +65,7 @@ void main() {
 
 }
 
-private static String genMemberInsertQuery(String name, String pwd, String email, String balance) {
+private String genMemberInsertQuery(String name, String pwd, String email, String balance) {
     return """
             INSERT INTO member
               ( name, password, email, balance )
